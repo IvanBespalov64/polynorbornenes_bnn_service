c9f123918e50a0c18bc3302681be1dd45db5c5f0
Ivan Bespalov <ivan.a.bespalov@gmail.com>
2026-06-13 19:56:07 +0000

    [fix] fix bugs, CORS still needs fix

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,17 @@ import typing
 import pathlib
 import pydantic
 
+import numpy as np
+
 from rdkit import Chem
 from rdkit.Chem import rdFingerprintGenerator
 
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
-from model import MTLBNNRegressor
+import torch
+from backend.model import MTLBNNRegressor
 
 class PredictionRequest(pydantic.BaseModel):
     smiles : str = pydantic.Field(...)
@@ -27,14 +32,20 @@ class PredictionRespone(pydantic.BaseModel):
     p_c3h8 : SinglePrediction
     p_c4h10 : SinglePrediction
 
-import os
-print(os.system('ls -lah'))
-print(os.system('pwd'))
-
 app = FastAPI()
 
-prediction_metadata_path = (pathlib.Path(__file__) / '..' / 'data' / 'prediction_metadata.json').resolve()
-model_checkpoint_path = (pathlib.Path(__file__) / '..' / 'data' / 'bnn_mtl_on_full_data.pt').resolve()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, replace "*" with your actual frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+prediction_metadata_path = (pathlib.Path(__file__) / '..' / '..' / 'data' / 'prediction_metadata.json').resolve()
+model_checkpoint_path = (pathlib.Path(__file__) / '..' / '..' / 'data' / 'bnn_mtl_on_full_data.pt').resolve()
+
+NUM_PRED_SAMPLES : int = 100
 
 prediction_metadata : typing.Dict[str, typing.Dict[str, float]] = None
 with open(prediction_metadata_path, 'r') as fp:
@@ -56,6 +67,69 @@ model = MTLBNNRegressor(
 )
 model.load_state_dict(torch.load(model_checkpoint_path))
 model.eval()
+
+@app.post('/predict')
+def predict(
+    request : PredictionRequest
+) -> PredictionRespone:
+
+    if Chem.MolFromSmiles(request.smiles) is None:
+        raise HTTPException(status_code=400, detail="Invalid SMILES!")
+
+    fps = generator.GetCountFingerprintAsNumPy(Chem.MolFromSmiles(request.smiles))
+    cur_x = torch.tensor(fps, dtype=torch.float32).view(1, -1)
+    
+    preds = {task : [] for task in prediction_metadata.keys()}
+    preds_mean, preds_std = {}, {}
+    with torch.inference_mode():
+        for task in prediction_metadata.keys():
+            for _ in range(NUM_PRED_SAMPLES):
+                preds[task].append(
+                    model(cur_x, task, sample=True).item()
+                )
+            preds_mean[task] = np.mean(preds[task]) * prediction_metadata[task]['target_std'] + prediction_metadata[task]['target_mean']
+            preds_std[task] = np.std(preds[task]) * prediction_metadata[task]['target_std'] + prediction_metadata[task]['target_mean']
+
+            preds_std[task] = max(0, np.sqrt(preds_std[task]**2 - prediction_metadata[task]['target_min_pred_std']**2))
+
+            # anti box-cox
+            preds_std[task] = np.exp(preds_std[task]) - 1
+            preds_mean[task] = np.exp(preds_mean[task]) - 1
+
+    return PredictionRespone(
+        p_n2=SinglePrediction(
+            mean=preds_mean['inhs_p(n2)'],
+            std=preds_std['inhs_p(n2)'],
+        ),
+        p_he=SinglePrediction(
+            mean=preds_mean['inhs_p(he)'],
+            std=preds_std['inhs_p(he)'],
+        ),
+        p_h2=SinglePrediction(
+            mean=preds_mean['inhs_p(h2)'],
+            std=preds_std['inhs_p(h2)'],
+        ),
+        p_o2=SinglePrediction(
+            mean=preds_mean['inhs_p(o2)'],
+            std=preds_std['inhs_p(o2)'],
+        ),
+        p_ch4=SinglePrediction(
+            mean=preds_mean['inhs_p(ch4)'],
+            std=preds_std['inhs_p(ch4)'],
+        ),
+        p_c2h6=SinglePrediction(
+            mean=preds_mean['inhs_p(c2h6)'],
+            std=preds_std['inhs_p(c2h6)'],
+        ),
+        p_c3h8=SinglePrediction(
+            mean=preds_mean['inhs_p(c3h8)'],
+            std=preds_std['inhs_p(c3h8)'],
+        ),
+        p_c4h10=SinglePrediction(
+            mean=preds_mean['inhs_p(c4h10)'],
+            std=preds_std['inhs_p(c4h10)'],
+        ),
+    )
 
 @app.get('/status')
 def status() -> int:
